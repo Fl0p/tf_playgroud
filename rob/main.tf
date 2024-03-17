ee8b3fdd0794bc74ebf3a9688a075e43d091f15f
Flop <flop@hackerspace.by>
2024-03-17 22:47:53 +0100

    Extract home

--- rob/main.tf
+++ rob/main.tf
@@ -14,20 +14,31 @@ resource "null_resource" "default" {
 }
 
 provider "docker" {
-  #host = "tcp://robMini:2376"
-  # host = "ssh://rob@robMini:22"
-  # ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
-  host = "unix:///var/run/docker.sock"
+  # host = "unix:///var/run/docker.sock"
+  # host = "tcp://robMini:2376"
+  host = "ssh://rob@robMini:22"
+  ssh_opts = [
+    # "-o", "StrictHostKeyChecking=no",
+    # "-o", "UserKnownHostsFile=/dev/null",
+  ]
+}
+
+resource "docker_volume" "shared_volume1" {
+  name = "shared_volume1"
 }
 
 resource "docker_image" "jenkins" {
   name         = "jenkins/jenkins:lts"
-  keep_locally = false
+  keep_locally = true
 }
 
 resource "docker_container" "jenkins" {
   image = docker_image.jenkins.image_id
   name  = "jenkins"
+  volumes {
+    container_path = "/var/jenkins_home"
+    host_path = "/Users/rob/jenkins_home"
+  }
   ports {
     internal = 8080
     external = 8080
