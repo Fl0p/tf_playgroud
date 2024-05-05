6e4bfff7478a5819feebc4a1bfbcdddbf4b19c3f
Flop <flop@hackerspace.by>
2024-05-05 22:31:34 +0200

    migrate new terraform

--- rob/main.tf
+++ rob/main.tf
@@ -1,17 +1,22 @@
 terraform {
-  backend "s3" {
-    endpoint                    = "https://robtfstate.fra1.digitaloceanspaces.com"
-    bucket                      = "robtfstate"
-    key                         = "rob/terraform.tfstate"
-    region                      = "eu-central-1"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    force_path_style            = true
-  }
-  # backend "local" {
-  #   path = ".tfstate/terraform.tfstate"
+  required_version = "~> 1.8"
+  # backend "s3" {
+  #   endpoints = {
+  #     s3 = "https://fra1.digitaloceanspaces.com"
+  #   }                    
+  #   bucket                      = "robtfstate"
+  #   key                         = "rob/terraform.tfstate"
+  #   region                      = "eu-central-1"
+  #   workspace_key_prefix        = "rob"
+  #   skip_credentials_validation = true
+  #   skip_requesting_account_id  = true
+  #   skip_metadata_api_check     = true
+  #   skip_region_validation      = true
+  #   use_path_style              = true
   # }
+  backend "local" {
+    path = ".tfstate/terraform.tfstate"
+  }
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
